d9e746ca95d440b0c6760d28b717d8e56084e781
MakinoHaruka <harukaxxxx@makinoworks.com>
2023-05-15 04:10:27 +0000

    test

--- blog.config.js
+++ blog.config.js
@@ -14,7 +14,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '牧野悠（ハルカ）', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '牧野悠（ハルカ）chatgpt-next-web', // 您的昵称 例如 tangly1024
   BIO:
     process.env.NEXT_PUBLIC_BIO ||
     'おまえが何かを望む時には、宇宙全体が協力して、それを実現するために助けてくれるのだよ。', // 作者简介
