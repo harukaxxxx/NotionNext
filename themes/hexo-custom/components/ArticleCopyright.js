c71880dde2f4b341e945e15e9885c8ad97695e5d
MakinoHaruka <harukaxxxx@makinoworks.com>
2026-06-03 00:47:47 +0800

    ✨ feat(copyright): prop copyright
    set copyright to notion prop author

--- themes/hexo-custom/components/ArticleCopyright.js
+++ themes/hexo-custom/components/ArticleCopyright.js
@@ -7,6 +7,7 @@ import { siteConfig } from '@/lib/config'
 import NotByAI from '@/components/NotByAI'
 
 export default function ArticleCopyright({ post }) {
+  console.log('post 物件:', post)
   const router = useRouter()
   const [path, setPath] = useState(siteConfig('LINK') + router.asPath)
   useEffect(() => {
@@ -25,7 +26,7 @@ export default function ArticleCopyright({ post }) {
         <li>
           <strong className='mr-2'>{locale.COMMON.AUTHOR}:</strong>
           <SmartLink href={'/about'} className='hover:underline'>
-            {siteConfig('AUTHOR')}
+            {post.author[0] ?? siteConfig('AUTHOR')}
           </SmartLink>
         </li>
         <li>
